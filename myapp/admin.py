dc281e43a69916d8db2482af2b713e12b8f5eebf
yerassyl013 <kaltbek2007@mail.ru>
2025-04-13 14:25:59 +0500

    Initial commit: Django Task project with admin panel

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at')  # поля в списке
+    search_fields = ('title',)              # строка поиска
+    list_filter = ('created_at',)           # фильтр справа
--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=255) # Название задачи
+    description = models.TextField(blank=True, null=True) # Описание
+    created_at = models.DateTimeField(auto_now_add=True) # Дата создания
+    def __str__(self):
+        return self.title
