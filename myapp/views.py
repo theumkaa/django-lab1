959e18cb83328d19a808150f19f1ec29f1bc7373
yerassyl013 <kaltbek2007@mail.ru>
2025-04-14 23:56:50 +0500

    Добавлен новый файл forms.py , Был введен корректировка в файле views.py, Были добавлены Настройка маршрутов и Ограничение доступа

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+from .forms import RegisterForm, UpdateProfileForm
 
-# Create your views here.
-def my_project():
-    return "my_project"
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = RegisterForm()
+    return render(request, 'register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'profile.html')
+
+@login_required
+def edit_profile(request):
+    if request.method == 'POST':
+        form = UpdateProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = UpdateProfileForm(instance=request.user)
+    return render(request, 'edit_profile.html', {'form': form})
